064be857d849f80766b4ec105a5d81366e18af18
Yusuke Suzuki <utatane.tea@gmail.com>
2015-11-27 02:36:54 +0900

    Customize cuda_add_executable_dc

--- cmake/CUDAMacros.cmake
+++ cmake/CUDAMacros.cmake
@@ -15,16 +15,38 @@ set(CUDA_C_OR_CXX CXX)
 #   linker_options      - Linker options.
 ##############################################################################
 ##############################################################################
-MACRO(CUDA_ADD_EXECUTABLE_DC target src_files dependencies options linker_options)
+MACRO(CUDA_ADD_EXECUTABLE_DC cuda_target src_files dependencies options linker_options)
   CUDA_ADD_CUDA_INCLUDE_ONCE()
 
+  set(CUDA_NVCC_INCLUDE_ARGS ${CUDA_NVCC_INCLUDE_ARGS_USER} "-I${CUDA_INCLUDE_DIRS}")
+  get_directory_property(CUDA_NVCC_INCLUDE_DIRECTORIES INCLUDE_DIRECTORIES)
+  list(REMOVE_DUPLICATES CUDA_NVCC_INCLUDE_DIRECTORIES)
+  if(CUDA_NVCC_INCLUDE_DIRECTORIES)
+    foreach(dir ${CUDA_NVCC_INCLUDE_DIRECTORIES})
+      list(APPEND CUDA_NVCC_INCLUDE_ARGS -I${dir})
+    endforeach()
+  endif()
+
+  # Separate the sources from the options
+  # CUDA_GET_SOURCES_AND_OPTIONS(_sources _cmake_options _options ${ARGN})
+  # Create custom commands and targets for each file.
+  # CUDA_WRAP_SRCS( ${cuda_target} OBJ _generated_files ${_sources} OPTIONS ${_options} )
+
   SET(obj_files "")
+  set(sources)
   FOREACH(f ${src_files})
-    GET_FILENAME_COMPONENT(obj_file ${f} NAME_WE)
+    get_filename_component(file_path "${f}" PATH)
+    if(IS_ABSOLUTE "${file_path}")
+      set(source_file "${f}")
+    else()
+      set(source_file "${CMAKE_CURRENT_SOURCE_DIR}/${f}")
+    endif()
+    list(APPEND sources ${source_file})
+    GET_FILENAME_COMPONENT(obj_file "${source_file}" NAME_WE)
     SET(obj_files "${obj_files};${obj_file}.o")
   ENDFOREACH(f ${src_files})
 
-  ADD_CUSTOM_TARGET(${target} ALL DEPENDS ${dependencies})
+  ADD_CUSTOM_TARGET(${cuda_target} ALL DEPENDS ${dependencies})
 
   # List of dependencies (static libraries)
   SET(dep_paths "")
@@ -43,14 +65,14 @@ MACRO(CUDA_ADD_EXECUTABLE_DC target src_files dependencies options linker_option
 
   # Compile and link the static device library with nvcc
   ADD_CUSTOM_COMMAND(
-    TARGET ${target}
+    TARGET ${cuda_target}
     PRE_BUILD
     COMMAND ${CMAKE_COMMAND} -E echo "Linking static device library to executable."
-    COMMAND ${CUDA_NVCC_EXECUTABLE} ${CUDA_NVCC_FLAGS} ${options} ${nvcc_flags} -dlink ${dep_paths} -c "${src_files}"
-    COMMAND ${CUDA_NVCC_EXECUTABLE} ${CUDA_NVCC_FLAGS} ${options} ${dep_paths} ${obj_files} ${linker_options} -o "${target}"
+    COMMAND ${CUDA_NVCC_EXECUTABLE} ${CUDA_NVCC_INCLUDE_ARGS} ${CUDA_NVCC_FLAGS} ${options} ${nvcc_flags} -dlink ${dep_paths} -c ${sources}
+    COMMAND ${CUDA_NVCC_EXECUTABLE} ${CUDA_NVCC_INCLUDE_ARGS} ${CUDA_NVCC_FLAGS} ${options} ${dep_paths} ${obj_files} ${linker_options} -o "${cuda_target}"
     )
 
-  SET_TARGET_PROPERTIES(${target}
+  SET_TARGET_PROPERTIES(${cuda_target}
     PROPERTIES
     LINKER_LANGUAGE ${CUDA_C_OR_CXX})
 ENDMACRO(CUDA_ADD_EXECUTABLE_DC)
