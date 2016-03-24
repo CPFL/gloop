3b628206213010fa67d1405cce31ec26ee875003
Yusuke Suzuki <utatane.tea@gmail.com>
2016-03-24 19:11:02 +0900

    Drop inl file

--- src/gloop/device_loop.cuh
+++ src/gloop/device_loop.cuh
@@ -81,6 +81,108 @@ private:
 };
 static_assert(std::is_trivially_destructible<DeviceLoop>::value, "DeviceLoop is trivially destructible");
 
+__device__ uint32_t DeviceLoop::position(DeviceCallback* callback)
+{
+    GLOOP_ASSERT_SINGLE_THREAD();
+    return callback - m_slots;
+}
+
+__device__ uint32_t DeviceLoop::position(IPC* ipc)
+{
+    GLOOP_ASSERT_SINGLE_THREAD();
+    return ipc - channel();
+}
+
+__device__ uint32_t DeviceLoop::position(DeviceContext::OnePage* page)
+{
+    GLOOP_ASSERT_SINGLE_THREAD();
+    return page - pages();
+}
+
+__device__ auto DeviceLoop::channel() const -> IPC*
+{
+    GLOOP_ASSERT_SINGLE_THREAD();
+    return m_deviceContext.channels + (GLOOP_BID() * GLOOP_SHARED_SLOT_SIZE);
+}
+
+__device__ auto DeviceLoop::context() const -> DeviceContext::PerBlockContext*
+{
+    GLOOP_ASSERT_SINGLE_THREAD();
+    return m_deviceContext.context + GLOOP_BID();
+}
+
+__device__ auto DeviceLoop::pages() const -> DeviceContext::OnePage*
+{
+    GLOOP_ASSERT_SINGLE_THREAD();
+    return m_deviceContext.pages + (GLOOP_BID() * GLOOP_SHARED_PAGE_COUNT);
+}
+
+template<typename Lambda>
+__device__ void DeviceLoop::allocOnePage(Lambda lambda)
+{
+    __shared__ void* page;
+    BEGIN_SINGLE_THREAD
+    {
+        page = nullptr;
+        int freePagePosPlusOne = __ffs(m_control.freePages);
+        if (freePagePosPlusOne == 0) {
+            uint32_t pos = enqueueSleep([lambda](DeviceLoop* loop, volatile request::Request* req) {
+                loop->allocOnePage(lambda);
+            });
+            m_control.pageSleepSlots |= (1ULL << pos);
+        } else {
+            int pagePos = freePagePosPlusOne - 1;
+            page = pages() + pagePos;
+            m_control.freePages &= ~(1UL << pagePos);
+        }
+    }
+    END_SINGLE_THREAD
+    if (!page)
+        return;
+    lambda(this, page);
+}
+
+template<typename Lambda>
+__device__ void DeviceLoop::enqueueLater(Lambda lambda)
+{
+    GLOOP_ASSERT_SINGLE_THREAD();
+    uint32_t pos = enqueueSleep(lambda);
+    m_control.wakeupSlots |= (1ULL << pos);
+}
+
+template<typename Lambda>
+__device__ uint32_t DeviceLoop::enqueueSleep(Lambda lambda)
+{
+    GLOOP_ASSERT_SINGLE_THREAD();
+    uint32_t pos = allocate(lambda);
+    m_control.sleepSlots |= (1ULL << pos);
+    return pos;
+}
+
+template<typename Lambda>
+__device__ IPC* DeviceLoop::enqueueIPC(Lambda lambda)
+{
+    GLOOP_ASSERT_SINGLE_THREAD();
+    uint32_t pos = allocate(lambda);
+    IPC* result = channel() + pos;
+    GPU_ASSERT(pos < GLOOP_SHARED_SLOT_SIZE);
+    return result;
+}
+
+template<typename Lambda>
+__device__ uint32_t DeviceLoop::allocate(Lambda lambda)
+{
+    GLOOP_ASSERT_SINGLE_THREAD();
+    int pos = __ffsll(m_control.freeSlots) - 1;
+    GPU_ASSERT(pos >= 0 && pos <= GLOOP_SHARED_SLOT_SIZE);
+    GPU_ASSERT(m_control.freeSlots & (1ULL << pos));
+    m_control.freeSlots &= ~(1ULL << pos);
+
+    new (m_slots + pos) DeviceCallback(lambda);
+    m_control.pending += 1;
+
+    return pos;
+}
+
 }  // namespace gloop
-#include "device_loop.inline.cuh"
 #endif  // GLOOP_DEVICE_LOOP_H_
