35202424d30166bc325d1c620d7b6293db434c5e
Yusuke Suzuki <utatane.tea@gmail.com>
2017-03-14 23:56:20 -0700

    iobench2: count

--- src/samples/iobench2/cp.cu
+++ src/samples/iobench2/cp.cu
@@ -1,6 +1,6 @@
 #include <gloop/gloop.h>
 
-__device__ void performCopy(gloop::DeviceLoop<>* loop, uchar* scratch, int zfd, size_t me, size_t filesize, int trials, int ioSize, int loopCount)
+__device__ void performCopy(gloop::DeviceLoop<>* loop, uchar* scratch, int zfd, size_t me, size_t filesize, int trials, int ioSize, int loopCount, int count)
 {
     volatile int res = 0;
     for (int i = 0; i < loopCount; ++i) {
@@ -14,7 +14,7 @@ __device__ void performCopy(gloop::DeviceLoop<>* loop, uchar* scratch, int zfd, 
                 assert(NULL);
             }
 
-            performCopy(loop, scratch, zfd, me + GLOOP_SHARED_PAGE_SIZE * loop->logicalGridDim().x, filesize, trials, ioSize, loopCount);
+            performCopy(loop, scratch, zfd, me + GLOOP_SHARED_PAGE_SIZE * loop->logicalGridDim().x, filesize, trials, ioSize, loopCount, count + 1);
         });
         return;
     }
@@ -34,7 +34,7 @@ __device__ void gpuMain(gloop::DeviceLoop<>* loop, char* src, int trials, int io
     gloop::fs::open(loop, src, O_RDONLY, [=](gloop::DeviceLoop<>* loop, int zfd) {
         gloop::fs::fstat(loop, zfd, [=](gloop::DeviceLoop<>* loop, int filesize) {
             size_t me = loop->logicalBlockIdx().x * GLOOP_SHARED_PAGE_SIZE;
-            performCopy(loop, scratch, zfd, me, filesize, trials, ioSize, loopCount);
+            performCopy(loop, scratch, zfd, me, filesize, trials, ioSize, loopCount, 0);
         });
     });
 }
