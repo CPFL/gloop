fe3f2e341a9620da24c0ba6bb57ac449b2c56431
Yusuke Suzuki <utatane.tea@gmail.com>
2017-11-09 10:46:33 +0900

    Adjust block numbers for P100 GPU, part 1

--- src/samples/rodinia/gloop/lavaMD/kernel/kernel_gpu_cuda_wrapper.cu
+++ src/samples/rodinia/gloop/lavaMD/kernel/kernel_gpu_cuda_wrapper.cu
@@ -66,7 +66,7 @@ void kernel_gpu_cuda_wrapper(par_str par_cpu,
     gloop::Statistics::instance().switchTo<gloop::Statistics::Type::GPUInit>();
     std::unique_ptr<gloop::HostLoop> hostLoop = gloop::HostLoop::create(0);
     // std::unique_ptr<gloop::HostContext> hostContext = gloop::HostContext::create(*hostLoop, dim3(30));
-    std::unique_ptr<gloop::HostContext> hostContext = gloop::HostContext::create(*hostLoop, dim3(40));
+    std::unique_ptr<gloop::HostContext> hostContext = gloop::HostContext::create(*hostLoop, dim3(112));
     // cudaThreadSynchronize();
 
     //====================================================================================================100
