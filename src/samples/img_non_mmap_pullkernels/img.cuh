25557429de5283a1803f201d8d054ffe5fc26109
Yusuke Suzuki <utatane.tea@gmail.com>
2018-04-04 03:54:02 +0900

    Add img_non_mmap_pullkernels

---
+++ src/samples/img_non_mmap_pullkernels/img.cuh
@@ -0,0 +1,35 @@
+/*
+  Copyright (C) 2016 Yusuke Suzuki <yusuke.suzuki@sslab.ics.keio.ac.jp>
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+  ARE DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+  THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+#ifndef IMG_CU_H_
+#define IMG_CU_H_
+
+#define GREP_ROW_WIDTH (4*1024)
+
+void __device__ img_gpu(
+        gloop::DeviceLoop<>* loop,
+        char* src, int src_row_len, int num_db_files, float match_threshold, int start_offset,
+        char* out, char* out2, char* out3, char* out4, char *out5, char* out6, char* out7);
+
+
+#endif  // IMG_CU_H_
---
+++ src/samples/img_non_mmap_pullkernels/img_gpu.cu
@@ -0,0 +1,320 @@
+/*
+ * This expermental software is provided AS IS.
+ * Feel free to use/modify/distribute,
+ * If used, please retain this disclaimer and cite
+ * "GPUfs: Integrating a file system with GPUs",
+ * M Silberstein,B Ford,I Keidar,E Witchel
+ * ASPLOS13, March 2013, Houston,USA
+ */
+
+#include <sys/mman.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <gloop/gloop.h>
+#include <gloop/debug.h>
+#include <gloop/utility/util.cu.h>
+#include "img.cuh"
+
+__device__ volatile gpunet::INIT_LOCK init_lock;
+__device__ volatile gpunet::LAST_SEMAPHORE last_lock;
+
+#define GLOOP_PAGE_SIZE (1 << 20)
+// #define GLOOP_PAGE_SIZE GLOOP_SHARED_PAGE_SIZE
+
+struct _pagehelper{
+    uchar* page;
+    size_t file_offset;
+    int isFilled;
+};
+
+struct Context {
+    float input_img_row[GREP_ROW_WIDTH];
+    char* db_files[6];
+    int* out_buffer;
+    float match_threshold;
+    int start_offset;
+    int total_rows;
+    int src_row_len;
+    int num_db_files;
+    int zfd_src;
+    _pagehelper ph_db;
+};
+
+template<typename Callback>
+__device__ GLOOP_NEVER_INLINE void get_row(gloop::DeviceLoop<>* loop, uchar* cur_page_ptr, size_t* cur_page_offset, int* isFilled, size_t req_file_offset, int max_file_size, int fd, Callback callback)
+{
+    if (*isFilled && *cur_page_offset+GLOOP_PAGE_SIZE>req_file_offset) {
+        callback(loop, (float*)(cur_page_ptr+(req_file_offset&(GLOOP_PAGE_SIZE-1))));
+        return;
+    }
+
+    int mapsize = (max_file_size-req_file_offset)>GLOOP_PAGE_SIZE?GLOOP_PAGE_SIZE:(max_file_size-req_file_offset);
+    BEGIN_SINGLE_THREAD
+    {
+        *cur_page_offset=(req_file_offset& (~(GLOOP_PAGE_SIZE-1)));// round to the beg. of the page
+        *isFilled = true;
+    }
+    END_SINGLE_THREAD
+    gloop::fs::read(loop, fd, *cur_page_offset, mapsize, (unsigned char*)cur_page_ptr, [=](gloop::DeviceLoop<>* loop, int bytesRead) {
+        callback(loop, (float*)(cur_page_ptr+(req_file_offset&(GLOOP_PAGE_SIZE-1))));
+    });
+}
+
+#define ACCUM_N 512
+__shared__ float s_reduction[ACCUM_N];
+
+GLOOP_ALWAYS_INLINE __device__ float inner_product(float* a, float* b, int size)
+{
+    float tmp=0;
+    //      __syncthreads();
+    //      if (threadIdx.x==0) {
+    //                      *res=0;
+    //      }
+    //      __syncthreads();
+    int i=0;
+    for( i=threadIdx.x;i<size;i+=blockDim.x){
+
+        tmp+=(a[i]-b[i])*(a[i]-b[i]);
+    }
+    s_reduction[threadIdx.x]=tmp;
+
+    __syncthreads();
+    for (int stride = ACCUM_N / 2; stride > 32; stride >>= 1)
+    {
+        if(threadIdx.x<stride) s_reduction[threadIdx.x] += s_reduction[stride + threadIdx.x];
+        __syncthreads();
+    }
+    for (int stride = 32; stride > 0 && threadIdx.x<32 ; stride >>=1 )
+    {
+        if(threadIdx.x<stride) s_reduction[threadIdx.x] += s_reduction[stride + threadIdx.x];
+    }
+    __syncthreads();
+
+    return s_reduction[0];
+}
+
+GLOOP_ALWAYS_INLINE __device__ bool match(float* a, float* b, int size, float match_threshold)
+{
+    return sqrt(inner_product(a,b,size)) < match_threshold;
+}
+
+template<typename Callback>
+void GLOOP_NEVER_INLINE __device__ process_one_row(gloop::DeviceLoop<>* loop, Context* context, int data_idx, int db_idx, int out_count, int db_size, int zfd_db, int _cursor, int db_rows, float* ptr_row_db, Callback callback)
+{
+    const int src_row_len = context->src_row_len;
+    const size_t file_offset = context->ph_db.file_offset;
+    const float match_threshold = context->match_threshold;
+    float* input_img_row = context->input_img_row;
+    int found = 0;
+    for (;_cursor < db_rows; _cursor += 1, ptr_row_db += src_row_len) {
+        size_t _req_offset=(_cursor*src_row_len)<<2;
+        if (_req_offset - file_offset >= GLOOP_PAGE_SIZE) {
+            get_row(loop, context->ph_db.page, &context->ph_db.file_offset, &context->ph_db.isFilled, _req_offset, db_size, zfd_db, [=] (gloop::DeviceLoop<>* loop, float* ptr_row_db) {
+                process_one_row(loop, context, data_idx, db_idx, out_count, db_size, zfd_db, _cursor, db_rows, ptr_row_db, callback);
+            });
+            return;
+        }
+
+        found = match(input_img_row, ptr_row_db, src_row_len, match_threshold);
+        if (found) {
+            break;
+        }
+        // This loop exit is not necessary since we frequently enter to loop state in get_row.
+        // if (gloop::loop::postTaskIfNecessary(loop, [=](gloop::DeviceLoop<>* loop) {
+        //         process_one_row(loop, context, data_idx, db_idx, out_count, db_size, zfd_db, _cursor + 1, db_rows, ptr_row_db + src_row_len, callback);
+        //     })) {
+        //     return;
+        // }
+    }
+
+    if (found) {
+        BEGIN_SINGLE_THREAD
+        {
+            context->out_buffer[out_count]=data_idx+context->start_offset*context->total_rows;
+            context->out_buffer[out_count+1]=db_idx;
+            context->out_buffer[out_count+2]=_cursor;
+        }
+        END_SINGLE_THREAD
+        gloop::loop::postTask(loop, [=](gloop::DeviceLoop<>* loop) {
+            callback(loop, ptr_row_db, 1);
+        });
+        return;
+    }
+
+    gloop::loop::postTask(loop, [=](gloop::DeviceLoop<>* loop) {
+        callback(loop, ptr_row_db, /* not found */ 0);
+    });
+}
+
+template<typename Callback>
+void GLOOP_NEVER_INLINE __device__ process_one_db(gloop::DeviceLoop<>* loop, Context* context, int data_idx, int db_idx, int out_count, Callback callback)
+{
+    if (db_idx < context->num_db_files) {
+        gloop::fs::open(loop, context->db_files[db_idx], /* O_RDONLY */ O_RDWR, [=](gloop::DeviceLoop<>* loop, int zfd_db) {
+            if (zfd_db<0) GPU_ERROR("Failed to open DB file");
+            gloop::fs::fstat(loop, zfd_db, [=](gloop::DeviceLoop<>* loop, size_t db_size) {
+                size_t db_rows=(db_size/context->src_row_len)>>2;
+
+                get_row(loop, context->ph_db.page, &context->ph_db.file_offset, &context->ph_db.isFilled, 0, db_size, zfd_db, [=](gloop::DeviceLoop<>* loop, float* ptr_row_db) {
+                    process_one_row(loop, context, data_idx, db_idx, out_count, db_size, zfd_db, 0, db_rows, ptr_row_db, [=](gloop::DeviceLoop<>* loop, float* ptr_row_db, int found) {
+                        context->ph_db.file_offset = 0;
+                        context->ph_db.isFilled = false;
+                        gloop::fs::close(loop, zfd_db, [=](gloop::DeviceLoop<>* loop, int error) {
+                            if (found) {
+                                callback(loop, found);
+                                return;
+                            }
+                            process_one_db(loop, context, data_idx, db_idx + 1, out_count, callback);
+                        });
+                    });
+                });
+            });
+        });
+        return;
+    }
+    context->ph_db.file_offset=0;
+    context->ph_db.isFilled=false;
+    callback(loop, /* found */ 0);
+}
+
+template<typename Callback>
+void __device__ process_one_data(gloop::DeviceLoop<>* loop, Context* context, size_t data_idx, int out_count, int limit, Callback callback)
+{
+    if (data_idx < limit) {
+        gloop::fs::read(loop, context->zfd_src, data_idx*context->src_row_len<<2, GREP_ROW_WIDTH*4, (uchar*)context->input_img_row, [=](gloop::DeviceLoop<>* loop, int bytes_read) {
+            if (bytes_read!=GREP_ROW_WIDTH*4) GPU_ERROR("Failed to read src");
+
+
+            int db_idx = 0;
+            context->ph_db.file_offset=0;
+            context->ph_db.isFilled=false;
+            process_one_db(loop, context, data_idx, db_idx, out_count, [=] (gloop::DeviceLoop<>* loop, int found) {
+                BEGIN_SINGLE_THREAD
+                {
+                    if (!found) {
+                        context->out_buffer[out_count]=data_idx+context->start_offset*context->total_rows;
+                        context->out_buffer[out_count+1]=-1;
+                        context->out_buffer[out_count+2]=-1;
+                    }
+                }
+                END_SINGLE_THREAD
+                // Increment
+                process_one_data(loop, context, data_idx + 1, out_count + 3, limit, callback);
+            });
+        });
+        return;
+    }
+    callback(loop);
+}
+
+void __device__ img_gpu(
+        gloop::DeviceLoop<>* loop,
+        char* src, int src_row_len, int num_db_files, float match_threshold, int start_offset,
+        char* out, char* out2, char* out3, char* out4, char *out5, char* out6, char* out7)
+{
+    src_row_len = GREP_ROW_WIDTH;
+    gloop::fs::open(loop, out, O_RDWR|O_CREAT, [=](gloop::DeviceLoop<>* loop, int zfd_o) {
+        if (zfd_o<0) GPU_ERROR("Failed to open output");
+
+        gloop::fs::open(loop, src, /* O_RDONLY */ O_RDWR, [=](gloop::DeviceLoop<>* loop, int zfd_src) {
+            if (zfd_src<0) GPU_ERROR("Failed to open input");
+
+            gloop::fs::fstat(loop, zfd_src, [=](gloop::DeviceLoop<>* loop, int in_size) {
+                int total_rows;
+                total_rows=(in_size/src_row_len)>>2;
+
+                int rows_per_chunk;
+                rows_per_chunk=total_rows/loop->logicalGridDim().x;
+                if (rows_per_chunk==0) rows_per_chunk=1;
+
+                int rows_to_process;
+                rows_to_process=rows_per_chunk;
+
+                if (loop->logicalBlockIdx().x==loop->logicalGridDim().x-1)
+                    rows_to_process=(total_rows - loop->logicalBlockIdx().x*rows_per_chunk);
+
+                __shared__ int toInit;
+                __shared__ Context* context;
+                BEGIN_SINGLE_THREAD
+                {
+                    context = (Context*)malloc(sizeof(Context));
+                    context->out_buffer=(int*)malloc(rows_to_process*sizeof(int)*3);
+                    context->db_files[0] = out2;
+                    context->db_files[1] = out3;
+                    context->db_files[2] = out4;
+                    context->db_files[3] = out5;
+                    context->db_files[4] = out6;
+                    context->db_files[5] = out7;
+                    context->match_threshold = match_threshold;
+                    context->start_offset = start_offset;
+                    context->total_rows = total_rows;
+                    context->src_row_len = src_row_len;
+                    context->num_db_files = num_db_files;
+                    context->ph_db = {(uchar*)malloc(GLOOP_PAGE_SIZE),0, false};
+                    context->zfd_src = zfd_src;
+
+                    toInit=init_lock.try_wait();
+                    if (toInit == 1)
+                    {
+                        // single_thread_ftruncate(zfd_o,0);
+                        __threadfence();
+                        init_lock.signal();
+                    }
+                }
+                END_SINGLE_THREAD
+
+                /*
+                   1. decide how many strings  each block does
+                   2. map input line
+                   3. map db
+                   4. scan through
+                   5. write to output
+                   */
+                int out_count=0;
+                int start=loop->logicalBlockIdx().x*rows_per_chunk;
+
+                process_one_data(loop, context, start, out_count, start + rows_to_process, [=] (gloop::DeviceLoop<>* loop) {
+                    //we are done.
+                    //write the output and finish
+                    //if (gmunmap(ptr_row_in,0)) GPU_ERROR("Failed to unmap input");
+                    int write_size=rows_to_process*sizeof(int)*3;
+                    gloop::fs::write(loop, zfd_o, loop->logicalBlockIdx().x*rows_per_chunk*sizeof(int)*3, write_size, (uchar*)context->out_buffer, [=](gloop::DeviceLoop<>* loop, int written_size) {
+                        if (written_size!=write_size) GPU_ERROR("Failed to write output");
+                        gloop::fs::close(loop, zfd_src, [=](gloop::DeviceLoop<>* loop, int error) {
+                            BEGIN_SINGLE_THREAD
+                            {
+                                free(context->out_buffer);
+                                free(context);
+                            }
+                            END_SINGLE_THREAD
+                            gloop::fs::close(loop, zfd_o, [=](gloop::DeviceLoop<>* loop, int error) { });
+                        });
+                    });
+                });
+            });
+        });
+    });
+}
+
+void init_device_app()
+{
+    // GLOOP_CUDA_SAFE_CALL(cudaDeviceSetLimit(cudaLimitPrintfFifoSize, 1<<25));
+    GLOOP_CUDA_SAFE_CALL(cudaDeviceSetLimit(cudaLimitMallocHeapSize, ((512) <<20)));
+    GLOOP_CUDA_SAFE_CALL(cudaDeviceSetLimit(cudaLimitStackSize, 4096));
+}
+
+void init_app()
+{
+    void* inited;
+
+    GLOOP_CUDA_SAFE_CALL(cudaGetSymbolAddress(&inited,init_lock));
+    GLOOP_CUDA_SAFE_CALL(cudaMemset(inited,0,sizeof(gpunet::INIT_LOCK)));
+
+    GLOOP_CUDA_SAFE_CALL(cudaGetSymbolAddress(&inited,last_lock));
+    GLOOP_CUDA_SAFE_CALL(cudaMemset(inited,0,sizeof(gpunet::LAST_SEMAPHORE)));
+}
+
+double post_app(double total_time, float trials)
+{
+    return 0;
+}
